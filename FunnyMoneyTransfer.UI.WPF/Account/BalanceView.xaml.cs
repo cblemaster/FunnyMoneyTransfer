0226640b246e11850e01557f7bda6a9884adf72f
Brian LeMaster <brian.lemaster@outlook.com>
2023-02-28 20:03:55 -0500

    Added balance view, viewmodel, and styles to WPF

---
+++ FunnyMoneyTransfer.UI.WPF/Account/BalanceView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FunnyMoneyTransfer.UI.WPF.Account
+{
+    /// <summary>
+    /// Interaction logic for BalanceView.xaml
+    /// </summary>
+    public partial class BalanceView : UserControl
+    {
+        public BalanceView() => InitializeComponent();
+    }
+}
---
+++ FunnyMoneyTransfer.UI.WPF/Account/BalanceViewModel.cs
@@ -0,0 +1,68 @@
+﻿using FunnyMoneyTransfer.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyMoneyTransfer.UI.WPF.Account
+{
+    public class BalanceViewModel : INotifyPropertyChanged
+    {
+        #region ctor
+        public BalanceViewModel()
+        {
+            Data.Account a = this._db.Accounts
+                                        .Include(n => n.StartingBalance)
+                                        .Include(t => t.TransferAccountIdToNavigations)
+                                        .Include(t => t.TransferAccountIdFromNavigations)
+                                        .FirstOrDefault(a => a.UserId == 2)!;
+            if (a != null)
+            {
+                this.CalculatedBalance = a.CalculatedBalance();
+                this.AsOfDate = DateTime.Now.ToString();
+            }
+        }
+        #endregion
+
+        #region fields
+        private readonly FunnyMoneyTransferContext _db = new();
+        private decimal _calculatedBalance;
+        private string _asOfDate = null!;
+        #endregion
+
+        #region events
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+        #endregion
+
+        #region properties       
+        public decimal CalculatedBalance
+        {
+            get =>_calculatedBalance;
+            set
+            {
+                if (value != _calculatedBalance)
+                {
+                    _calculatedBalance = value;
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(CalculatedBalance)));
+                }                
+            }
+        }
+
+        public string AsOfDate
+        {
+            get => _asOfDate;
+            set
+            {
+                if (value != _asOfDate)
+                {
+                    _asOfDate = value;
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(AsOfDate)));
+                }
+            }
+        }
+        #endregion
+    }
+}
