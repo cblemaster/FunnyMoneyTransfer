b71dd63258a880625fcfe1fa37f0da6958a796d3
Brian LeMaster <brian.lemaster@outlook.com>
2023-03-03 16:07:59 -0500

    List users view and viewmodel in WPF

---
+++ FunnyMoneyTransfer.UI.WPF/User/ListUsersView.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Controls;
+using System.Collections;
+using System.Linq;
+
+namespace FunnyMoneyTransfer.UI.WPF.User
+{
+    /// <summary>
+    /// Interaction logic for ListUsersView.xaml
+    /// </summary>
+    public partial class ListUsersView : UserControl
+    {
+        public ListUsersView() => InitializeComponent();
+
+        private void lvUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListUsersViewModel vm = (ListUsersViewModel)this.DataContext;
+            MainWindowViewModel mwvm = ((MainWindowViewModel)(App.Current.MainWindow).DataContext);
+
+            if (e.AddedItems.Count > 0 && e.AddedItems[0] is Data.User selectedUser)
+            {
+                foreach (Data.User user in vm.Users)
+                {
+                    if (user.Id == selectedUser.Id && user.Id != mwvm.LoggedInUser.Id)
+                        user.ShowSendAndRequestButtons = true;
+                    else
+                        user.ShowSendAndRequestButtons = false;
+                }
+            }
+        }
+    }
+}
---
+++ FunnyMoneyTransfer.UI.WPF/User/ListUsersViewModel.cs
@@ -0,0 +1,41 @@
+﻿using FunnyMoneyTransfer.Data;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+
+namespace FunnyMoneyTransfer.UI.WPF.User
+{
+    public class ListUsersViewModel : INotifyPropertyChanged
+    {
+        #region ctor
+        public ListUsersViewModel()
+        {
+            this.Users = new ObservableCollection<Data.User>(_db.Users.OrderBy(u => u.Username).ToList());
+        }
+        #endregion
+
+        #region fields
+        private readonly FunnyMoneyTransferContext _db = new();
+        private ObservableCollection<Data.User> _users = null!;
+        #endregion
+
+        #region events
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+        #endregion
+
+        #region properties
+        public ObservableCollection<Data.User> Users
+        {
+            get => _users;
+            set
+            {
+                if (value != _users)
+                {
+                    _users = value;
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(Users)));
+                }
+            }
+        }
+        #endregion
+    }
+}
