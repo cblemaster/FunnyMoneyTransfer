f628fc634a70305bcf9f56e0d85658fb33d842c9
Brian LeMaster <brian.lemaster@outlook.com>
2023-03-03 17:49:27 -0500

    Added send transfer view and viewmodel in WPF

---
+++ FunnyMoneyTransfer.UI.WPF/Transfer/SendTransferViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace FunnyMoneyTransfer.UI.WPF.Transfer
+{
+    public class SendTransferViewModel : INotifyPropertyChanged
+    {
+        #region ctor
+        public SendTransferViewModel()
+        {
+
+        }
+        #endregion
+
+        #region fields
+        private decimal _amount;
+        #endregion
+
+        #region events
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+        #endregion
+
+        #region properties
+        public decimal Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                if (value != _amount)
+                {
+                    _amount = value;
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(Amount)));
+                }
+            }
+        }
+        #endregion        
+    }
+}
