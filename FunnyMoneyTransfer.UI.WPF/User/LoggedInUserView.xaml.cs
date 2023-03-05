aac65a9ff7bccdca2793a97edcf333513d2d0af5
Brian LeMaster <brian.lemaster@outlook.com>
2023-03-05 12:56:43 -0500

    Added logged in user view and viewmodel in WPF

---
+++ FunnyMoneyTransfer.UI.WPF/User/LoggedInUserView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FunnyMoneyTransfer.UI.WPF.User
+{
+    /// <summary>
+    /// Interaction logic for LoggedInUserView.xaml
+    /// </summary>
+    public partial class LoggedInUserView : UserControl
+    {
+        public LoggedInUserView() => InitializeComponent();
+    }
+}
---
+++ FunnyMoneyTransfer.UI.WPF/User/LoggedInUserViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunnyMoneyTransfer.UI.WPF.User
+{
+    public class LoggedInUserViewModel : INotifyPropertyChanged
+    {
+        #region ctor
+        public LoggedInUserViewModel()
+        {
+
+        }
+        #endregion
+
+        #region fields
+        private Data.User _loggedInUser = null!;
+        #endregion
+
+        #region events
+        public event PropertyChangedEventHandler? PropertyChanged = delegate { };
+        #endregion
+
+        #region properties
+        public Data.User LoggedInUser
+        {
+            get => _loggedInUser;
+            set
+            {
+                if (value != _loggedInUser)
+                {
+                    _loggedInUser = value;
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(LoggedInUser)));
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(IsUserLoggedIn)));
+                    this.PropertyChanged!(this, new PropertyChangedEventArgs(nameof(IsNoUserLoggedIn)));
+                }
+            }
+        }
+
+        public bool IsUserLoggedIn
+        {
+            get => this.LoggedInUser != null && this.LoggedInUser is Data.User;
+        }
+
+        public bool IsNoUserLoggedIn
+        {
+            get => this.LoggedInUser == null;
+        }
+        #endregion
+    }
+}
